5bd0a6eb66558445f1ab96c92b8b1280eaa141b5
Jacob Leonardo <leonardojacob257@gmail.com>
2025-05-25 11:20:40 -0600

    git ignore

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "21fe730a47abcb1f"; }
+/******/ 		__webpack_require__.h = function() { return "a859a7a3d0acc689"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
